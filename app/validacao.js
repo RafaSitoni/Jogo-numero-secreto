14ab01ed0e6528bc7a1587c1072bcc112d4a414f
Rafaela Sitoni <146780642+RafaSitoni@users.noreply.github.com>
2024-03-30 17:49:58 -0300

    adicionando o jogo numero secreto

---
+++ app/validacao.js
@@ -0,0 +1,47 @@
+function verificaValorValido(chute){
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return
+    }
+
+    if (numeroMaiorMenorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>
+            Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}
+        </div>`;
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+            <img class="trofeu" src="img/trofeu.png">
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <div ontouchstart="">
+                <div class="button">
+                    <a href="#" id="jogar-novamente">Jogar Novamente</a>
+                </div>
+            </div>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+    }
+    
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorMenorPermitido(numero){
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
